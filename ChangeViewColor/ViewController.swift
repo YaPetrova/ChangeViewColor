2571027b591549c0cee124b86bddf92872f085e3
Yana <yanka.pol@gmail.com>
2022-04-27 22:50:38 +0700

    storyboard

--- ChangeViewColor/ViewController.swift
+++ ChangeViewColor/ViewController.swift
@@ -8,34 +8,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
+    @IBOutlet var colorView: UIView!
+    
     @IBOutlet var redSlider: UISlider!
-    
     @IBOutlet var greenSlider: UISlider!
-    
     @IBOutlet var blueSlider: UISlider!
-    
     
     @IBOutlet var redValueLabel: UILabel!
     @IBOutlet var greenValueLabel: UILabel!
     @IBOutlet var blueValueLabel: UILabel!
-    
-    
-    
- 
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colorView.layer.cornerRadius = 10
         
         redValueLabel.text = String(redSlider.value)
         greenValueLabel.text = String(greenSlider.value)
         blueValueLabel.text = String(blueSlider.value)
         
+        colorView.backgroundColor = UIColor(
+            red: (Double(redValueLabel.text ?? "0") ?? 0),
+            green: (Double(greenValueLabel.text ?? "0") ?? 0),
+            blue: (Double(blueValueLabel.text ?? "0") ?? 0),
+            alpha: 1)
     }
+        
+    @IBAction func redAddingValue() {
+        redValueLabel.text = String(round((redSlider.value)*10)/10)
+    }
+    
+    @IBAction func greenAddingValue() {
+        greenValueLabel.text = String(round((greenSlider.value)*10)/10)
+    }
+    
+    @IBAction func blueAddingValue() {
+        blueValueLabel.text = String(round((blueSlider.value)*10)/10)
+    }
+    
     @IBAction func redAddingSlider() {
-    
+        colorView.backgroundColor = UIColor(
+            red: (Double(redValueLabel.text ?? "0") ?? 0),
+            green: (Double(greenValueLabel.text ?? "0") ?? 0),
+            blue: (Double(blueValueLabel.text ?? "0") ?? 0),
+            alpha: 1)
     }
     
-   
+    @IBAction func greenAddingSlider() {
+        colorView.backgroundColor = UIColor(
+            red: (Double(redValueLabel.text ?? "0") ?? 0),
+            green: (Double(greenValueLabel.text ?? "0") ?? 0),
+            blue: (Double(blueValueLabel.text ?? "0") ?? 0),
+            alpha: 1)
+    }
+    
+    @IBAction func blueAddingSlider() {
+        colorView.backgroundColor = UIColor(
+            red: (Double(redValueLabel.text ?? "0") ?? 0),
+            green: (Double(greenValueLabel.text ?? "0") ?? 0),
+            blue: (Double(blueValueLabel.text ?? "0") ?? 0),
+            alpha: 1)
+    }
 }
-
+    
